5293fe774da5f028e72fdb24e158c2a6ad9a80af
Kxyaaad <823942471@qq.com>
2019-10-14 11:46:43 +0800

    Initial Commit

---
+++ Weib_Demo/Classes/View/Message/WBMessageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WBMessageViewController.swift
+//  Weib_Demo
+//
+//  Created by SanDisk on 2019/10/14.
+//  Copyright © 2019 SanDisk. All rights reserved.
+//
+
+import UIKit
+
+class WBMessageViewController: WBBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weib_Demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Weib_Demo
+//
+//  Created by SanDisk on 2019/10/12.
+//  Copyright © 2019 SanDisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
