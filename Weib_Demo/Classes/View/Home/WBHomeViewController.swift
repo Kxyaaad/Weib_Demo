f332902c7e0becf73c9765d1aa747316f5026eb8
Kxyaaad <823942471@qq.com>
2019-10-28 17:51:19 +0800

    添加刷线控件

--- Weib_Demo/Classes/View/Home/WBHomeViewController.swift
+++ Weib_Demo/Classes/View/Home/WBHomeViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
+private let cellID = "cellID"
+
 class WBHomeViewController: WBBaseViewController {
 
+    private lazy var statusList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func loadData() {
+        for i in 0..<10 {
+            statusList.insert(i.description, at: 0)
+        }
+        refreshControl?.endRefreshing()
     }
     
 
@@ -28,5 +39,19 @@ extension WBHomeViewController {
     override func setUpUI() {
         super.setUpUI()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", target: self, action: #selector(showFriends))
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+}
+
+extension WBHomeViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return statusList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.textLabel?.text = statusList[indexPath.row]
+        return cell
     }
 }
--- Weib_Demo/Classes/View/Main/WBBaseViewController.swift
+++ Weib_Demo/Classes/View/Main/WBBaseViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class WBBaseViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
+class WBBaseViewController: UIViewController{
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = String(indexPath.row)
-        return cell
-    }
     
-    lazy var tableView = UITableView()
+    var tableView : UITableView?
+    var refreshControl: UIRefreshControl?
+    
+    @objc func loadData() {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +31,27 @@ extension WBBaseViewController {
         view.backgroundColor = UIColor.cz_random()
         
         tableView = UITableView(frame: CGRect(x: 0, y: UIApplication.shared.statusBarFrame.height + 44 , width: UIScreen.cz_screenWidth(), height: UIScreen.cz_screenHeight() - (UIApplication.shared.statusBarFrame.height + 44 + (tabBarController?.tabBar.frame.height)!)))
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        view.addSubview(tableView)
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        //添加刷新攻坚
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(loadData), for: .valueChanged)
+        tableView?.addSubview(refreshControl!)
+        
+        view.addSubview(tableView!)
+        loadData()
+    }
+}
+
+extension WBBaseViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
  
