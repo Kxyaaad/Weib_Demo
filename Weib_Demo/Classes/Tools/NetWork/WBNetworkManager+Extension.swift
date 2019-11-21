d81ccc4ccbdd74cfe118cc5df74b099da546fc12
Kxyaaad <823942471@qq.com>
2019-11-21 17:16:14 +0800

    token过期处理

--- Weib_Demo/Classes/Tools/NetWork/WBNetworkManager+Extension.swift
+++ Weib_Demo/Classes/Tools/NetWork/WBNetworkManager+Extension.swift
@@ -12,12 +12,20 @@ extension WBNetworkManager {
     
     func statusList(completion:@escaping (_ list:[[String:Any]]?, _ isSuccess:Bool)->()) {
         let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
-        request(Method: .GET, URLString: urlString, parameter: ["access_token":"2.00YWlWBHJj2S5D05171bc783dOKFBB"]) { (json, bool) in
+        
+       
+        
+        tokenRequest(Method: .GET, URLString: urlString, parameter: nil) { (json, bool) in
            
-            let list = (json as! [String:Any])["statuses"] as! [[String:Any]]
-            let isSuccess = bool
-//            print("最终结果", list)
-            completion(list, isSuccess)
+            if bool == true {
+                let list = (json as! [String:Any])["statuses"] as! [[String:Any]]
+                            let isSuccess = bool
+                //            print("最终结果", list)
+                            completion(list, isSuccess)
+            }else {
+                print(json)
+            }
+            
         }
         
     }
--- Weib_Demo/Classes/Tools/NetWork/WBNetworkManager.swift
+++ Weib_Demo/Classes/Tools/NetWork/WBNetworkManager.swift
@@ -17,26 +17,54 @@ enum WBHTTPMethod {
 
 class WBNetworkManager: AFHTTPSessionManager {
     
-    ///封装AFN的GET/POST请求
+    //封装AFN的GET/POST请求
     static let shared = WBNetworkManager()
+    //访问令牌
+    var accessToken: String? = "2.00YWlWBHJj2S5D05171bc783dOKFBB"
+    
+    
+    //负责拼接token的网络请求
+    func tokenRequest(Method:WBHTTPMethod, URLString: String, parameter:[String:Any]?, completion: @escaping (_ json:Any?, _ isSuccess:Bool)->()) {
+        
+        //如果accessToken为nil,则直接返回
+        guard accessToken != nil else { return
+            completion(nil, false)
+        }
+        //如果为nil，则新建一个字典
+        var parameter = parameter
+        if parameter == nil {
+            parameter = [String:Any]()
+        }
+        parameter!["access_token"] = accessToken
+        //调用request发起真正的网络请求方法
+        request(Method: Method, URLString: URLString, parameter: parameter, completion: completion)
+    }
+    
     
     
     //使用一个函数封装GET/POST请求
     
-    func request(Method:WBHTTPMethod, URLString: String, parameter:[String:Any], completion: @escaping (_ json:Any?, _ isSuccess:Bool)->() ) {
+    func request(Method:WBHTTPMethod, URLString: String, parameter:[String:Any]?, completion: @escaping (_ json:Any?, _ isSuccess:Bool)->() ) {
         
         switch Method {
         case .GET:
             get(URLString, parameters: parameter, progress: nil, success: { (URLSessionDataTask, json) -> () in
                 completion(json, true)
-            }) { (URLSessionDataTask, error) -> () in
-                completion(error, false)
+            }) { (task, error) -> () in
+               //针对403处理用户token过期
+                if (task?.response as? HTTPURLResponse)?.statusCode == 403 {
+                    print("token过期")
+                }
+            
+                //失败回调
+//                print("错误信息",error)
+                completion(nil, false)
             }
         default:
             post(URLString, parameters: parameter, progress: nil, success: { (URLSessionDataTask, json) -> () in
                 completion(json, true)
             }) { (URLSessionDataTask, error) in
-                completion(error, false)
+                completion(nil, false)
             }
         }
     }
