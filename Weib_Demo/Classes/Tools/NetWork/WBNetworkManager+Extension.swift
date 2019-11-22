34457ad459b2721fbf6962c61dd65859c7effd1b
Kxyaaad <823942471@qq.com>
2019-11-22 16:10:48 +0800

    下拉刷新

--- Weib_Demo/Classes/Tools/NetWork/WBNetworkManager+Extension.swift
+++ Weib_Demo/Classes/Tools/NetWork/WBNetworkManager+Extension.swift
@@ -8,14 +8,17 @@
 
 import Foundation
 
+//since_id: 下拉刷新是需要的最上面一条微博的ID，默认为0
+//max_id: 上拉加载时，最下面一条微博的ID，默认为0
+
 extension WBNetworkManager {
     
-    func statusList(completion:@escaping (_ list:[[String:Any]]?, _ isSuccess:Bool)->()) {
+    func statusList(since_id:Int64 = 0, max_id:Int64 = 0, completion:@escaping (_ list:[[String:Any]]?, _ isSuccess:Bool)->()) {
         let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
-        
+        let parameters = ["since_id":since_id, "max_id":max_id]
        
         
-        tokenRequest(Method: .GET, URLString: urlString, parameter: nil) { (json, bool) in
+        tokenRequest(Method: .GET, URLString: urlString, parameter: parameters) { (json, bool) in
            
             if bool == true {
                 let list = (json as! [String:Any])["statuses"] as! [[String:Any]]
--- Weib_Demo/Classes/ViewModel/WBStatusListViewModel.swift
+++ Weib_Demo/Classes/ViewModel/WBStatusListViewModel.swift
@@ -14,7 +14,10 @@ class WBStatusListModel:WBStatus {
     lazy var statusList = [WBStatus]()
     //加载微博列表
     func loadStatus(completion:@escaping (_ isSuccess:Bool)->()) {
-        WBNetworkManager.shared.statusList { (list, isSuccess) in
+        
+        let since_id = statusList.first?.id ?? 0
+        
+        WBNetworkManager.shared.statusList(since_id: since_id, max_id: 0) { (list, isSuccess) in
             //字典转模型
             guard let array  = NSArray.yy_modelArray(with: WBStatus.self, json: list ?? []) as? [WBStatus] else {
                 completion(isSuccess)
@@ -22,7 +25,8 @@ class WBStatusListModel:WBStatus {
             }
             
             //拼接数据
-            self.statusList += array
+            //下拉刷新时，应该拼接数组
+            self.statusList = array + self.statusList
             
             //完成回调
             completion(isSuccess)
